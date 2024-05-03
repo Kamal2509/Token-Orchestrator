69a40366a3b0e4e65a0a3c59443e4b1a3b5681b5
Kamal Sharma <kamalsharma25091994@gmail.com>
2024-05-03 18:32:32 +0530

    after dockerfile

--- sample-project/src/main/java/com/example/sample/project/SampleProjectApplication.java
+++ sample-project/src/main/java/com/example/sample/project/SampleProjectApplication.java
@@ -1,8 +1,5 @@
 package com.example.sample.project;
 
-import java.util.Optional;
-import java.util.function.Consumer;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
